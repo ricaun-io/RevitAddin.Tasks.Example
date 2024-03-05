c2e41af3edbc44dc952be314d61b32cbf6c5d0cc
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2024-03-05 17:43:26 -0300

    Add Tasks Package

--- RevitAddin.Tasks.Example/Revit/App.cs
+++ RevitAddin.Tasks.Example/Revit/App.cs
@@ -1,6 +1,7 @@
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using ricaun.Revit.UI;
+using ricaun.Revit.UI.Tasks;
 
 namespace RevitAddin.Tasks.Example.Revit
 {
@@ -8,8 +9,13 @@ namespace RevitAddin.Tasks.Example.Revit
     public class App : IExternalApplication
     {
         private static RibbonPanel ribbonPanel;
+        private static RevitTaskService revitTaskService;
+        public static IRevitTask RevitTask => revitTaskService;
         public Result OnStartup(UIControlledApplication application)
         {
+            revitTaskService = new RevitTaskService(application);
+            revitTaskService.Initialize();
+
             ribbonPanel = application.CreatePanel("RevitAddin.Tasks.Example");
             ribbonPanel.CreatePushButton<Commands.Command>()
                 .SetLargeImage("Resources/Revit.ico");
@@ -18,6 +24,8 @@ namespace RevitAddin.Tasks.Example.Revit
 
         public Result OnShutdown(UIControlledApplication application)
         {
+            revitTaskService?.Dispose();
+
             ribbonPanel?.Remove();
             return Result.Succeeded;
         }
--- RevitAddin.Tasks.Example/Revit/Commands/Command.cs
+++ RevitAddin.Tasks.Example/Revit/Commands/Command.cs
@@ -1,6 +1,8 @@
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using ricaun.Revit.UI.Tasks;
+using System.Threading.Tasks;
 
 namespace RevitAddin.Tasks.Example.Revit.Commands
 {
@@ -10,6 +12,15 @@ namespace RevitAddin.Tasks.Example.Revit.Commands
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elementSet)
         {
             UIApplication uiapp = commandData.Application;
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+                await App.RevitTask.Run((uiapp) =>
+                {
+                    TaskDialog.Show("Revit", $"Hello from RevitTask {uiapp.ActiveUIDocument.Document.Title}");
+                });
+            });
 
             return Result.Succeeded;
         }
